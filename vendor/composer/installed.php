57dd865fe9f341665deb4a620a557ceb9b381d3c
Kayvan-Ehtesh <kayvan@fittedrunning.com>
2021-01-21 14:43:11 -0700

    Removed Composer from the .gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4465c1cec94d37349f3db7235f8206fac62a2a48',
+    'name' => 'usefulteam/jwt-auth',
+  ),
+  'versions' => 
+  array (
+    'firebase/php-jwt' => 
+    array (
+      'pretty_version' => 'v5.2.0',
+      'version' => '5.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'feb0e820b8436873675fd3aca04f3728eb2185cb',
+    ),
+    'usefulteam/jwt-auth' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4465c1cec94d37349f3db7235f8206fac62a2a48',
+    ),
+  ),
+);
